7ddabfed82bb0405d426584b826d65e5a785eef9
Thanh Nguyen <lethanh300504@gmail.com>
2025-06-01 13:49:46 +1000

    extract javadoc

---
+++ LLM_Test_Gen/Scripts/info-extractor/src/main/java/org/example/InfoExtractorRunner.java
@@ -0,0 +1,265 @@
+package org.example;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParserConfiguration;
+import javassist.compiler.ast.MethodDecl;
+import sootup.core.Project;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.model.Method;
+import sootup.core.model.SootMethod;
+import sootup.core.model.SootClass;
+import sootup.core.views.View;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaProject;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.JavaSootClassSource;
+import sootup.java.core.language.JavaLanguage;
+import sootup.core.types.Type;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.*;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InfoExtractorRunner {
+    private static final String TEST_DATA_RELATIVE_PATH = "../../Data/Test_Data.csv";
+    private static final String LANG_1_BUGGY_SRC_RELATIVE_PATH = "../../../lang_1_buggy/src/main/java";
+    private static final String CSV_DELIMITER = ",";
+//    private static final String CSV_HEADER = "\"FQN\",\"Signature\",\"Jimple Code Representation\"";
+    private static final String[] CSV_COLUMNS = {
+            "FQN",
+            "Signature",
+            "Jimple Code Representation",
+            "Method Modifiers",
+            "Annotations",
+//            "Java Doc",
+//            "Class Context",
+//            "Class-level Fields",
+//            "Control-Flow Graph",
+//            "Parameter Constraints",
+//            "External Dependencies",
+//            "Literal Constants"
+    };
+    private static final String CSV_HEADER = InfoExtractorRunner.getCsvHeader();
+    private static final JavaParser parser = createJavaParser();
+
+
+    public static void main(String[] args) {
+        String targetClassesPathToLang1Buggy = "../../../lang_1_buggy/target/classes";
+
+        Path moduleBasePath = Paths.get(".").toAbsolutePath().normalize();
+        Path outputCsvAbsPath = moduleBasePath.resolve(TEST_DATA_RELATIVE_PATH).normalize();
+
+        // Point SootUp to the compiled classes
+        AnalysisInputLocation<JavaSootClass> inputLocation =
+                new JavaClassPathAnalysisInputLocation(targetClassesPathToLang1Buggy);
+
+        // Build the project (using Java 8 here)
+        JavaLanguage language = new JavaLanguage(8);
+        Project project = JavaProject.builder(language)
+                .addInputLocation(inputLocation)
+                .build();
+
+        // Create a View for reading class/method data
+        View view = project.createView();
+
+        // Retrieve all classes discovered by SootUp
+        Collection<SootClass<JavaSootClassSource>> allClasses = view.getClasses();
+
+        // Define the targeted classes
+        Set<String> targetClassesNames = Set.of(
+                "org.apache.commons.lang3.CharRange",
+                "org.apache.commons.lang3.CharSetUtils",
+                "org.apache.commons.lang3.text.WordUtils",
+                "org.apache.commons.lang3.text.translate.CodePointTranslator",
+                "org.apache.commons.lang3.concurrent.AtomicInitializer"
+        );
+
+
+        // Iterate over each class
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(outputCsvAbsPath.toString())))  {
+            writer.println(CSV_HEADER);
+
+            for (SootClass<JavaSootClassSource> sootClass : allClasses) {
+                String fullClassName = sootClass.getName();
+
+                // If this is the runner class or the methods isn't from CharRange, CharSetUtils, or WordUtils, skip it
+                if (fullClassName.equals("org.example.InfoExtractorRunner") || !targetClassesNames.contains(fullClassName)) {
+                    continue;
+                }
+
+                // Iterate over each method in the class
+                for (SootMethod method : sootClass.getMethods()) {
+                    // Only proceed if the method is concrete
+                    if (!method.isConcrete() || method.getName().equals("<init>") || method.getName().equals("<clinit>") || method.getName().startsWith("access$")) {
+                        continue;
+                    }
+
+                    // Extract method name, parameter types, and return type
+                    String methodName = method.getName();
+                    List<Type> paramTypes = method.getParameterTypes();
+                    Type returnType = method.getReturnType();
+
+                    String paramListString = paramTypes.stream()
+                            .map(Type::toString)
+                            .collect(Collectors.joining(", "));
+
+                    StringBuilder fqnStringBuilder = new StringBuilder();
+                    StringBuilder signatureStringBuilder = new StringBuilder();
+
+                    // Construct the FQN
+                    fqnStringBuilder
+                            .append(fullClassName)
+                            .append(".")
+                            .append(methodName)
+                            .append("(")
+                            .append(paramListString)
+                            .append(")");
+
+                    // Construct the signature
+                    signatureStringBuilder
+                            .append(returnType)
+                            .append(" ")
+                            .append(methodName)
+                            .append("(")
+                            .append(paramListString)
+                            .append(")");
+
+                    // Retrieve the Jimple code
+                    String jimpleCode = method.getBody().toString();
+
+                    String quotedFqn = quoteField(fqnStringBuilder.toString());
+                    String quotedSignature = quoteField(signatureStringBuilder.toString());
+                    String quotedJimpleCode = quoteField(jimpleCode);
+
+                    // Retrieve the Method Modifiers
+                    String classPath = fullClassName.replace('.', File.separatorChar);
+                    Path srcPath = Paths.get(
+                            LANG_1_BUGGY_SRC_RELATIVE_PATH,
+                            classPath + ".java"
+                    );
+
+                    CompilationUnit compilationUnit = parser.parse(srcPath)
+                            .getResult()
+                            .orElseThrow(() -> new RuntimeException("Unable to parse: " + srcPath));
+
+                    ClassOrInterfaceDeclaration classOrInterfaceDeclaration = compilationUnit
+                            .getClassByName(sootClass.getName().substring(sootClass.getName().lastIndexOf('.') + 1))
+                            .orElseThrow(() -> new RuntimeException("Class not found: " + sootClass.getName()));
+
+                    Integer paramCount = paramTypes.size();
+
+                    MethodDeclaration methodDeclaration = classOrInterfaceDeclaration.getMethodsByName(methodName)
+                            .stream()
+                            .filter(m ->
+                                    m.getParameters().size() == method.getParameterCount()
+                            )
+                            .findFirst()
+                            .orElseThrow(() -> new RuntimeException("Method not found: " + methodName));
+
+                    // Get the modifiers from the Soot Method
+                    String modifiers = method.getModifiers().stream()
+                            .map(Enum::name)
+                            .collect(Collectors.joining(" "));
+
+                    // Get annotations
+                    String annotations = methodDeclaration.getAnnotations().stream()
+                            .map(a -> a.getNameAsString())
+                            .collect(Collectors.joining(" "));
+
+
+
+                    String quotedModifiers = quoteField(modifiers);
+                    String quotedAnnotations = quoteField(annotations);
+
+
+                    String rawJavaDoc = methodDeclaration.getJavadoc()
+                            .map(javadoc -> {
+                                StringBuilder sb = new StringBuilder();
+                                sb.append(javadoc.getDescription().toText().trim());
+
+                                javadoc.getBlockTags().forEach(blockTag -> {
+                                    sb.append(System.lineSeparator());
+                                    sb.append("@").append(blockTag.getTagName());
+                                    blockTag.getName().ifPresent(name -> sb.append(" ").append(name));
+                                    sb.append(" ").append(blockTag.getContent().toText().trim());
+                                });
+                                return sb.toString();
+                            }).orElse("");
+
+                    String quotedJavaDoc = quoteField(rawJavaDoc);
+
+
+                    String csvLine = String.join(
+                            CSV_DELIMITER,
+                            quotedFqn,
+                            quotedSignature,
+                            quotedJimpleCode,
+                            quotedModifiers,
+                            quotedAnnotations,
+                            quotedJavaDoc
+                    );
+                    writer.println(csvLine);
+
+                    // Print the information
+                    System.out.println("-----------------------------------------------------------------");
+                    System.out.println("CODE INFORMATION:\n");
+                    System.out.println("- FQN: " + fqnStringBuilder);
+                    System.out.println("- Signature: " + signatureStringBuilder);
+                    System.out.println("- Jimple Code:\n" + jimpleCode);
+                    System.out.println("- Method Modifiers: " + modifiers);
+                    System.out.println("- Annotations: " + annotations);
+                    System.out.println("- JavaDoc: " + rawJavaDoc);
+                    System.out.println("-----------------------------------------------------------------");
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("An error occurred while processing the classes: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static String quoteField(String data) {
+        if (data == null) {
+            return "\"\"";
+        }
+        String escapedData = data.replace("\"", "\"\"");
+        return "\"" + escapedData + "\"";
+    }
+
+    private static String getCsvHeader() {
+        return Stream.of(CSV_COLUMNS)
+                .map(col -> "\"" + col + "\"")
+                .collect(Collectors.joining(CSV_DELIMITER));
+    }
+
+    private static JavaParser createJavaParser() {
+        CombinedTypeSolver typeSolver = new CombinedTypeSolver();
+        typeSolver.add(new ReflectionTypeSolver());
+        typeSolver.add(new JavaParserTypeSolver((new File(LANG_1_BUGGY_SRC_RELATIVE_PATH))));
+
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+
+        ParserConfiguration config = new ParserConfiguration().setSymbolResolver(symbolSolver);
+
+        return new JavaParser(config);
+
+    }
+}
